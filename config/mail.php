fbd068a1fe0dacf40674ad5c08d68bb03250ef69
Jean Gomes <jeangp2006@gmail.com>
2018-07-18 01:11:18 -0300

    mail clean config

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.sendgrid.net'),
+    'host' => env('MAIL_HOST', ''),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'caveirasdamontanha@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Caveiras da Montanha'),
+        'address' => env('MAIL_FROM_ADDRESS', ''),
+        'name' => env('MAIL_FROM_NAME', ''),
     ],
 
     /*
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', ''),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => 'jeangomes',
+    'username' => '',
 
-    'password' => 'send*mail*1517',
+    'password' => '',
 
     /*
     |--------------------------------------------------------------------------
