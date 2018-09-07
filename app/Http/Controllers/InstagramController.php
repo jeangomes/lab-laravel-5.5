ac14d76e312feb9640960f353d4e3d461e1664ac
Jean Gomes <jeangp2006@gmail.com>
2018-09-06 23:03:26 -0300

    get fotos from instagram

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -10,14 +10,10 @@ class InstagramController extends Controller
 
     public function index()
     {
-// Create a new instagram instance.
-        //$instagram = new Instagram('544314600.569b83a.b686f25c29384d3e80e36f40bce9db43'); jean
-        $instagram = new Instagram(''); //caveiras
 
-// Fetch recent user media items.
+        $instagram = new Instagram(env('INSTA_ID', 'YOUR-ID-INSTA'));
+        // Fetch recent user media items.
         //$instagram->media();
-
-// Fetch user information.
         $instagrams = $instagram->media();
         return view('site.fotos')->with(compact('instagrams'));
     }
